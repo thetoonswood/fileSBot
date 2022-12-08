10ebc190ce069a99cb0f41da05ffff07b6fc5c49
thetoonswood <118585979+thetoonswood@users.noreply.github.com>
2022-12-08 10:12:54 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -37,8 +37,25 @@ async def batch(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://telegram.me/{client.username}?start={base64_string}"
     link2 = f"https://telegram.dog/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}<br>{link2}", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([
+        [
+                InlineKeyboardButton(
+                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "📥 Generated Link", url=f"{link}"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "📥 Generated Link - 2", url=f"{link2}"
+                )
+            ]
+        ]
+    )
+    await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
@@ -58,5 +75,22 @@ async def link_generator(client: Client, message: Message):
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://telegram.me/{client.username}?start={base64_string}"
     link2 = f"https://telegram.dog/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}<br>{link2}", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([
+        [
+                InlineKeyboardButton(
+                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "📥 Generated Link", url=f"{link}"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "📥 Generated Link - 2", url=f"{link2}"
+                )
+            ]
+        ]
+    )
+    await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
